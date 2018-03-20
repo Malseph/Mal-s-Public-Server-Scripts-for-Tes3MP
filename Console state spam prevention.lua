be46bdebc667d932a5f0e6509b5404ca9e4d7167
Malseph <33076374+Malseph@users.noreply.github.com>
2018-03-20 09:40:55 -0500

    Update Console state spam prevention.lua

--- Console state spam prevention.lua
+++ Console state spam prevention.lua
@@ -1,4 +1,6 @@
 --I did my best to "convert" this to a version everyone else could use. This was put together quickly, but it works fine.
+--Don't bother using this if you don't understand how to modify anything yourself. 
+--You will need 1. A delete packet function, and 2. To modify SaveObjectStates so that "ConsoleSpamPrevention" can get what it depends on
 
 Methods.ConsoleSpamPrevention = function(pid, refIndex, refId, cellDescription)
 	local Name = refId.."-Spam"
@@ -10,14 +12,14 @@ Methods.ConsoleSpamPrevention = function(pid, refIndex, refId, cellDescription)
 	else	
 		Players[pid].StateSpam[Name] = (Players[pid].StateSpam[Name] + 1)
 		if Players[pid].StateSpam[Name] >= 5 then
-			--Insert your own cell load check here. Make sure its loaded. I would add mine, but it is full of extra stuff.
-			--Insert Your own Delete packet here for the RefIndex! I would add mine, but is full of extra stuff.
+			--Optional: Insert your own cell load check here. Make sure its loaded. I would add mine, but it is full of extra stuff.
+			--Insert Your own Delete packet here for the RefIndex! I would add mine, but is full of extra stuff. Just make sure you only send it to the player that sets that state false (As seen below)
 			tes3mp.LogMessage(1, "- " .. refIndex .. " with refId: "..refId.." was causing spam and has been deleted")			
 		end
 	end	
 end
 
---Add this to SaveObjectStates in the cell base file
+--Add this to SaveObjectStates in the cell base file and modify SaveObjectStates
 if state == false then
 	myMod.ConsoleSpamPrevention(pid, refIndex, refId, self.description)
 end		
